f8bbaada48ab8c768852fa0117d16cf898ff57ee
haduchau321 <83698436+haduchau321@users.noreply.github.com>
2021-05-05 17:46:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 from pydantic import BaseModel
 
 class myghi(BaseModel):
@@ -22,6 +21,3 @@ async def submit(out:myghi):
     print(name,passwrod,trangthai)
     return out,True
 
-
-if __name__ == '__main__':
-    uvicorn.run(app)
